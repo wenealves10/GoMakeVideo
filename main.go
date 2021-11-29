162d00b1ef78189e75d020c56f9fed4f4ae501cf
Wene Alves <weneplay5@gmail.com>
2021-11-29 19:29:48 +0100

    aaaaa

--- main.go
+++ main.go
@@ -2,59 +2,57 @@ package main
 
 import (
 	"fmt"
-	"io"
-	"log"
-	"net/http"
-	"os"
+	"io/ioutil"
+	"path/filepath"
+	"sort"
 
+	"github.com/icza/mjpeg"
 	"github.com/sikang99/cinema"
 )
 
 func main() {
-	// checkErr := func(err error) {
-	// 	if err != nil {
-	// 		panic(err)
-	// 	}
-	// }
-
-	// outName := "file.mp4"
-	// aw, err := mjpeg.New(outName, 1336, 700, 4)
-	// checkErr(err)
-
-	// // Create a movie from images:
-	// matches, err := filepath.Glob("*.jpg")
-	// checkErr(err)
-	// sort.Strings(matches)
-
-	// fmt.Println("Found images:", matches)
-	// for _, name := range matches {
-	// 	data, err := ioutil.ReadFile(name)
-	// 	checkErr(err)
-	// 	checkErr(aw.AddFrame(data))
-	// }
-
-	// checkErr(aw.Close())
-	// fmt.Printf("%s was written successfully.\n", outName)
-
-	// loading the test video
-	fmt.Println("Downloading Test Video...")
-	video_url := "https://media.w3.org/2010/05/sintel/trailer.mp4"
-	if err := DownloadFile("test.mp4", video_url); err != nil {
-		panic(err)
+	checkErr := func(err error) {
+		if err != nil {
+			panic(err)
+		}
 	}
 
+	outName := "file.mp4"
+	aw, err := mjpeg.New(outName, 1336, 700, 1)
+	checkErr(err)
+
+	// Create a movie from images:
+	matches, err := filepath.Glob("*.jpg")
+	checkErr(err)
+	sort.Strings(matches)
+
+	fmt.Println("Found images:", matches)
+	for _, name := range matches {
+		data, err := ioutil.ReadFile(name)
+		checkErr(err)
+		checkErr(aw.AddFrame(data))
+	}
+
+	checkErr(aw.Close())
+	fmt.Printf("%s was written successfully.\n", outName)
+
+	// loading the test video
+	// fmt.Println("Downloading Test Video...")
+	// video_url := "https://media.w3.org/2010/05/sintel/trailer.mp4"
+	// if err := DownloadFile("test.mp4", video_url); err != nil {
+	// 	panic(err)
+	// }
+
 	// // initializing the test video as a cinema video object
-	v, err := cinema.MakeVideo("test.mp4")
+	v, err := cinema.MakeVideo("file.mp4")
 	if err != nil {
 		fmt.Println(err)
 	}
 
 	// testing all setters
-	v.Trim(0, 10)
-	v.SetStart(0)
-	v.SetEnd(10)
+	// v.Trim(0, 10)
 	v.SetSize(400, 400)
-	v.Render("test_output.mov")
+	v.Render("test_output.mp4")
 
 	// testing all getters
 	fmt.Println(v.Filepath())
@@ -66,29 +64,29 @@ func main() {
 
 }
 
-func DownloadFile(filepath string, url string) error {
-	// check if the file exist already
-	finfo, err := os.Stat(filepath)
-	if err != nil {
-		return err
-	}
-	log.Println(finfo.Name(), finfo.Size())
+// func DownloadFile(filepath string, url string) error {
+// 	// check if the file exist already
+// 	finfo, err := os.Stat(filepath)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	log.Println(finfo.Name(), finfo.Size())
 
-	// Get the data
-	resp, err := http.Get(url)
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
+// 	// Get the data
+// 	resp, err := http.Get(url)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	defer resp.Body.Close()
 
-	// Create the file
-	out, err := os.Create(filepath)
-	if err != nil {
-		return err
-	}
-	defer out.Close()
+// 	// Create the file
+// 	out, err := os.Create(filepath)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	defer out.Close()
 
-	// Write the body to file
-	_, err = io.Copy(out, resp.Body)
-	return err
-}
+// 	// Write the body to file
+// 	_, err = io.Copy(out, resp.Body)
+// 	return err
+// }
