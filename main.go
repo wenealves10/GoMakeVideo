193cdf6580e83d366fda280db5e36e2ee7fce51e
Wene Alves <weneplay5@gmail.com>
2021-11-29 20:30:03 +0100

    asss

--- main.go
+++ main.go
@@ -21,7 +21,6 @@ func main() {
 	aw, err := mjpeg.New(outName, 1336, 700, 1)
 	checkErr(err)
 
-	// Create a movie from images:
 	matches, err := filepath.Glob("*.jpg")
 	checkErr(err)
 	sort.Strings(matches)
@@ -36,26 +35,15 @@ func main() {
 	checkErr(aw.Close())
 	fmt.Printf("%s Baixou com sucesso\n", outName)
 
-	// loading the test video
-	// fmt.Println("Downloading Test Video...")
-	// video_url := "https://media.w3.org/2010/05/sintel/trailer.mp4"
-	// if err := DownloadFile("test.mp4", video_url); err != nil {
-	// 	panic(err)
-	// }
-
-	// // initializing the test video as a cinema video object
 	v, err := cinema.Load("file.mp4")
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	// testing all setters
-	// v.Trim(0, 10)
 	v.SetStart(0)
 	v.SetEnd(10)
 	v.Render("file_modificar.mp4")
 
-	// testing all getters
 	fmt.Println(v.Filepath())
 	fmt.Println(v.Start())
 	fmt.Println(v.End())
@@ -64,30 +52,3 @@ func main() {
 	fmt.Println(v.Duration())
 
 }
-
-// func DownloadFile(filepath string, url string) error {
-// 	// check if the file exist already
-// 	finfo, err := os.Stat(filepath)
-// 	if err != nil {
-// 		return err
-// 	}
-// 	log.Println(finfo.Name(), finfo.Size())
-
-// 	// Get the data
-// 	resp, err := http.Get(url)
-// 	if err != nil {
-// 		return err
-// 	}
-// 	defer resp.Body.Close()
-
-// 	// Create the file
-// 	out, err := os.Create(filepath)
-// 	if err != nil {
-// 		return err
-// 	}
-// 	defer out.Close()
-
-// 	// Write the body to file
-// 	_, err = io.Copy(out, resp.Body)
-// 	return err
-// }
